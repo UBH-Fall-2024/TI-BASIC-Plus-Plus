113b4456f7bd9348a75773c26d3944c6ab52faa8
Peter Lilley <hi.petelilley@gmail.com>
2024-11-09 18:29:56 -0500

    Ran lexer from driver

--- tools/driver/src/main.c
+++ tools/driver/src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-#include <ti-basic-plus-plus/basic/diagnostics.h>
 #include <driver/options.h>
+#include <ti-basic-plus-plus/basic/diagnostics.h>
+#include <ti-basic-plus-plus/basic/input_file.h>
+#include <ti-basic-plus-plus/lexer/lexer.h>
 
 #include <assert.h>
 #include <stb_ds.h>
@@ -36,11 +38,38 @@ int main(int argc, const char** argv) {
 }
 
 static void compile(void) {
-  // Lexing
+  input_file_t input_file;
+
+  if (!if_init(&input_file, driver_config.input_path)) {
+    diag_report_file(&d, ERROR, driver_config.input_path, "failed to open file");
+    return;
+  }
+
+  token_t* head_token = NULL;
+  /* ast_node_t* ast_root = NULL; */
+
+  // Lexical analysis
+
+  head_token = tokenize_file(&input_file, &d);
+  if (head_token == NULL) {
+    return;
+  }
+  if (should_exit(&d)) {
+    goto CLEANUP;
+  }
+
+  if (driver_config.dump_tokens) {
+    print_token_list(head_token, stdout);
+  }
 
   // Parsing
 
   // Semantic analysis
 
   // (Optional) Encoding
+
+CLEANUP:
+  token_list_destroy(head_token);
+  if_destroy(&input_file);
 }
+
